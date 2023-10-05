8c2e3dd2a521153c2e7b90c775e4a66954799e7a
qafulov <qafulovh@gmail.com>
2023-10-05 12:31:45 +0400

    Initilazion api created

--- App.js
+++ App.js
@@ -4,10 +4,18 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({
-    hello: "king is back",
-  });
+app.get("/api/v1/init", (req, res) => {
+  try {
+    res.status(200).json({
+      error: false,
+      message: "App initialization is successful!",
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: true,
+      message: "App initialization is unsuccessful!",
+    });
+  }
 });
 
 app.listen(3001, () => {
