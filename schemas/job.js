af4ee81187e8ca6e0aa98340d2ea0475d4471357
qafulov <qafulovh@gmail.com>
2023-10-23 13:16:10 +0400

    Added new schema change for job

--- schemas/job.js
+++ schemas/job.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 
 const Job = new mongoose.Schema(
   {
+    category: {
+      type: String,
+      required: true,
+    },
     title: {
       type: String,
       required: true,
