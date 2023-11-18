e14a3115df5839f53d46a2049c8979e84a8b6226
qafulov <qafulovh@gmail.com>
2023-11-18 17:33:57 +0400

    Added api which return count of notifications

--- controllers/notifications.js
+++ controllers/notifications.js
@@ -49,4 +49,44 @@ const getAllMyNotifications = async (req, res) => {
     });
   }
 };
-module.exports = { createNotification, getAllMyNotifications };
+const getNotificationsCount = async (req, res) => {
+  try {
+    const { email } = req.user;
+    if (!email) {
+      return res.status(404).json({
+        error: true,
+        message: "Kimlik doğrulama başarısız",
+      });
+    }
+
+    const myUser = await user.findOne({ email: email });
+    if (!myUser) {
+      return res.status(404).json({
+        error: true,
+        message: "Kullanıcı bulunamadı",
+      });
+    }
+    const notificationsList = myUser.notifications; // Sadece iş kimliklerini alıyoruz
+    // İş kimliklerini kullanarak iş nesnelerini çekiyoruz
+    const allNotifications = await notifications
+      .find({ _id: { $in: notificationsList } })
+      .sort({ createdAt: -1 });
+    const countOfNotifications = allNotifications.filter(
+      (one) => one.status === "close"
+    );
+    res.status(200).json({
+      error: false,
+      data: countOfNotifications.length,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: true,
+      message: error.message,
+    });
+  }
+};
+module.exports = {
+  createNotification,
+  getAllMyNotifications,
+  getNotificationsCount,
+};
--- routes/notifications.js
+++ routes/notifications.js
@@ -1,8 +1,13 @@
 const express = require("express");
-const { getAllMyNotifications } = require("../controllers/notifications");
+const {
+  getAllMyNotifications,
+  getNotificationsCount,
+} = require("../controllers/notifications");
 const { verifyJwt } = require("../middleware/jwt");
 
 const routes = express.Router();
 
 routes.get("/", verifyJwt, getAllMyNotifications);
+routes.get("/count", verifyJwt, getNotificationsCount);
+
 module.exports = routes;
