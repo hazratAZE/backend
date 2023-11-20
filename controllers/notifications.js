79198c70f71dcb0afa6334bad52b7478ad6fecbc
qafulov <qafulovh@gmail.com>
2023-11-20 14:09:41 +0400

    Added notif opn all,delete all,delete one function

--- controllers/notifications.js
+++ controllers/notifications.js
@@ -16,6 +16,7 @@ const createNotification = async (title, body, image, onPress, type) => {
     console.log(error);
   }
 };
+
 const getAllMyNotifications = async (req, res) => {
   try {
     const { email } = req.user;
@@ -107,6 +108,141 @@ const openNotification = async (req, res) => {
     });
   }
 };
+const openAllMyNotifications = async (req, res) => {
+  try {
+    const { email } = req.user;
+
+    if (!email) {
+      return res.status(404).json({
+        error: true,
+        message: "Kimlik doğrulama başarısız",
+      });
+    }
+
+    const myUser = await user
+      .findOne({ email: email })
+      .populate("notifications");
+
+    if (!myUser) {
+      return res.status(404).json({
+        error: true,
+        message: "Kullanıcı bulunamadı",
+      });
+    }
+
+    // Update the status of all notifications to "open" in the Notification model
+    await notifications.updateMany(
+      { _id: { $in: myUser.notifications.map((notif) => notif._id) } },
+      { $set: { status: "open" } }
+    );
+
+    // Update the status of all notifications in myUser.notifications array
+    myUser.notifications.forEach((notification) => {
+      notification.status = "open";
+    });
+
+    await myUser.save();
+
+    return res.status(200).json({
+      error: false,
+      message: "All notifications marked as read!",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      message: error.message,
+    });
+  }
+};
+
+const deleteNotification = async (req, res) => {
+  try {
+    const { email } = req.user;
+    const { id } = req.body;
+
+    if (!email) {
+      return res.status(404).json({
+        error: true,
+        message: "Kimlik doğrulama başarısız",
+      });
+    }
+
+    const myUser = await user
+      .findOne({ email: email })
+      .populate("notifications");
+
+    if (!myUser) {
+      return res.status(404).json({
+        error: true,
+        message: "Kullanıcı bulunamadı",
+      });
+    }
+
+    // Find the notification and remove it
+    const notificationIndex = myUser.notifications.findIndex(
+      (notif) => notif._id.toString() === id
+    );
+
+    if (notificationIndex !== -1) {
+      myUser.notifications.splice(notificationIndex, 1); // Remove the notification from the array
+      await myUser.save();
+
+      // Delete the notification document from the Notification model
+      return res.status(200).json({
+        error: false,
+        message: "Notification deleted!",
+      });
+    } else {
+      return res.status(404).json({
+        error: true,
+        message: "Bildirim bulunamadı",
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      message: error.message,
+    });
+  }
+};
+const deleteAllNotifications = async (req, res) => {
+  try {
+    const { email } = req.user;
+
+    if (!email) {
+      return res.status(404).json({
+        error: true,
+        message: "Kimlik doğrulama başarısız",
+      });
+    }
+
+    const myUser = await user
+      .findOne({ email: email })
+      .populate("notifications");
+
+    if (!myUser) {
+      return res.status(404).json({
+        error: true,
+        message: "Kullanıcı bulunamadı",
+      });
+    }
+
+    // Remove all notifications from the user's notifications array
+    myUser.notifications = [];
+    await myUser.save();
+
+    // Delete all notifications associated with the user from the Notification model
+    return res.status(200).json({
+      error: false,
+      message: "All notifications deleted!",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      message: error.message,
+    });
+  }
+};
 
 const getNotificationsCount = async (req, res) => {
   try {
@@ -149,4 +285,7 @@ module.exports = {
   getAllMyNotifications,
   getNotificationsCount,
   openNotification,
+  deleteNotification,
+  deleteAllNotifications,
+  openAllMyNotifications,
 };
--- routes/notifications.js
+++ routes/notifications.js
@@ -3,6 +3,9 @@ const {
   getAllMyNotifications,
   getNotificationsCount,
   openNotification,
+  deleteNotification,
+  deleteAllNotifications,
+  openAllMyNotifications,
 } = require("../controllers/notifications");
 const { verifyJwt } = require("../middleware/jwt");
 
@@ -11,5 +14,8 @@ const routes = express.Router();
 routes.get("/", verifyJwt, getAllMyNotifications);
 routes.get("/count", verifyJwt, getNotificationsCount);
 routes.post("/open", verifyJwt, openNotification);
+routes.post("/delete", verifyJwt, deleteNotification);
+routes.post("/deleteAll", verifyJwt, deleteAllNotifications);
+routes.post("/openAll", verifyJwt, openAllMyNotifications);
 
 module.exports = routes;
