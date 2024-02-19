148a8c05421d7d6e577e35fbaf84e824c88699d0
qafulov <qafulovh@gmail.com>
2024-02-19 19:28:30 +0400

    Version name changed

--- routes/appinfo.js
+++ routes/appinfo.js
@@ -21,7 +21,7 @@ routes.get("/version", (req, res) => {
   try {
     res.status(200).json({
       error: false,
-      data: "1.0",
+      data: "1.0.0",
     });
   } catch (error) {
     res.status(500).json({
