4475bb6ce35672721db63425739b84eb1e5c6df2
qafulov <qafulovh@gmail.com>
2024-05-17 22:01:49 +0400

    New version added

--- routes/appinfo.js
+++ routes/appinfo.js
@@ -21,7 +21,7 @@ routes.get("/version", (req, res) => {
   try {
     res.status(200).json({
       error: false,
-      data: "1.0.4",
+      data: "1.0.6",
     });
   } catch (error) {
     res.status(500).json({
