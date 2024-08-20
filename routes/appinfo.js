a204367554210835a62477c9d5f3c7d93b427476
qafulov <qafulovh@gmail.com>
2024-08-20 12:40:57 +0400

    Version changed from 1.2.4 to 1.2.5

--- routes/appinfo.js
+++ routes/appinfo.js
@@ -21,7 +21,7 @@ routes.get("/version", (req, res) => {
   try {
     res.status(200).json({
       error: false,
-      data: "1.2.3",
+      data: "1.2.5",
     });
   } catch (error) {
     res.status(500).json({
